d543309285fd7b04ce4f36cad374f2fe27aab6f2
ccgjosh <ccgjosh@gmail.com>
2017-03-13 08:58:49 -0400

    Update select2 field
    There seems to be a bug when removing all items from select2, so code
    was added to manually remove final items.

--- fields/zestsms-select2/js/select2-settings.js
+++ fields/zestsms-select2/js/select2-settings.js
@@ -1,39 +1,56 @@
 (function($){
 
-  ZestSMSSelect2 = {
-    _init: function()
-    {
-      var $el = $('select.zestsms-select2');
+	ZestSMSSelect2 = {
+		_init: function()
+		{
+			$('select.zestsms-select2').each(function() {
+				$(this).select2({
+					width: '100%',
+				}).on('change', $.proxy(ZestSMSSelect2._maybePreview, this.context))
+				.on('select2:unselecting', function(e) {
+		          $(this).data('unselecting', true);
+		        }).on('select2:open', function(e) { // note the open event is important
+		          if ($(this).data('unselecting')) {
+		            $(this).removeData('unselecting'); // you need to unset this before close
+		            $(this).select2('close');
+		          }
+		        });
 
-      $el.select2({
-        width: '100%',
-      });
+				if($(this).attr('multiple')) {
+					var ul = $(this).siblings('.select2-container').first('ul.select2-selection__rendered');
+					ul.sortable({
+						placeholder : 'ui-state-highlight',
+						forcePlaceholderSize: true,
+						items       : 'li:not(.select2-search__field)',
+						tolerance   : 'pointer',
+						stop: function() {
+							$($(ul).find('.select2-selection__choice').get().reverse()).each(function() {
+								var id = $(this).data('data').id;
+								var option = $(this).find('option[value="' + id + '"]')[0];
+								$(this).prepend(option);
+							});
+						}
+					});
+				}
+			});
+		},
 
-      if($el.attr('multiple')) {
-        var ul = $el.next('.select2-container').first('ul.select2-selection__rendered');
-        ul.sortable({
-          placeholder : 'ui-state-highlight',
-          forcePlaceholderSize: true,
-          items       : 'li:not(.select2-search__field)',
-          tolerance   : 'pointer',
-          stop: function() {
-            $($(ul).find('.select2-selection__choice').get().reverse()).each(function() {
-              var id = $(this).data('data').id;
-              var option = $el.find('option[value="' + id + '"]')[0];
-              $el.prepend(option);
-            });
-          }
-        });
-      }
-    }
-  }
+		_maybePreview: function() {
+			var e = {
+				target: this
+			};
+			
+			var field       = $(this).closest('.fl-field');
+			var previewType = field.data('preview');
 
-  $(function(){
-    var initSettingsForms = FLBuilder._initSettingsForms;
-    FLBuilder._initSettingsForms = function() {
-      ZestSMSSelect2._init();
-      initSettingsForms();
-    };
-  });
+			if ('refresh' == previewType.type) {
+				FLBuilder.preview.delayPreview(e);
+			}
+		}
+	}
+
+	FLBuilder.addHook('settings-form-init', function() {
+		ZestSMSSelect2._init();
+	});
 
 })(jQuery);
