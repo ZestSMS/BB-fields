4fee488d407673c55c4b897ab15b90cca747b7c5
ccgjosh <ccgjosh@gmail.com>
2017-05-31 14:58:46 -0400

    Default value fix

--- fields/zestsms-measurement/zestsms-measurement.php
+++ fields/zestsms-measurement/zestsms-measurement.php
@@ -32,7 +32,7 @@ function zestsms_measurement_field( $name, $value, $field, $settings ) {
 
 	if(!isset($field['placeholder'])) $field['placeholder'] = '0';
 	if(!isset($field['default'])) $field['default'] = 'auto';
-	if(!isset($value)) $value = 'auto';
+	if(!isset($value) || $value == '') $value = 'auto';
 
 	foreach($units as $unit=>$param) {
 		// Define toggle
