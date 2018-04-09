efff7f30ac8627ec529207e5c498ebbebd3122e1
zestjosh <zestjosh@users.noreply.github.com>
2018-04-09 10:23:52 -0400

    Misc Updates

--- fields/zestsms-measurement/js/zestsms-measurement.js
+++ fields/zestsms-measurement/js/zestsms-measurement.js
@@ -1,37 +1,23 @@
 (function($){
 
-    ZestSMSMeasurement = {
-        _init: function()
-        {
-            $('.zestsms-measurement').on('input keyup change', function(){
-				setTimeout($.proxy(ZestSMSMeasurement._updateAndPreview, this), 0);
-            });
-        },
+	ZestSMSMeasurement = {
+    _init: function()
+    {
+			$('body').delegate('.zestsms-measurement', 'focusout', function(){
+        var field_value,
+        measurement_input = $(this).find('input.measurement-input'),
+				measurement_unit = $(this).find('select.measurement-unit');
 
-		_updateAndPreview: function()
-		{
-			var e = {
-				target: this
-			};
+        field_value = $('option:selected', measurement_unit).text();
+        if(measurement_input.not(':hidden')) field_value = $.trim(measurement_input.val()) + field_value;
 
-            var field_value,
-            	measurement_input = $(this).find('input.measurement-input'),
-            	measurement_unit = $(this).find('select.measurement-unit');
-
-            field_value = $('option:selected', measurement_unit).val();
-            if(measurement_input.css('display') != 'none') {
-				var field_measurement = (measurement_input.val()) ? $.trim(measurement_input.val()) : '0';
-                field_value = field_measurement + field_value;
-            }
-
-            $(this).find('input.field-value').val(field_value);
-
-			FLBuilder.preview.delayPreview(e);
-		}
+        $(this).find('input.field-value').val(field_value);
+      });
     }
+  }
 
-	FLBuilder.addHook('settings-form-init', function() {
-	    ZestSMSMeasurement._init();
-	});
+  $(function(){
+      ZestSMSMeasurement._init();
+  });
 
 })(jQuery);
